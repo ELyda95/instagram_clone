a25cfea729ce6aaef4c68ba3d1c7d1e2c30d6a0a
Derek <derekkiminho@gmail.com>
2020-12-02 15:44:55 -0800

    Completing models for users, posts, likes, and comments.

--- app/models/user.py
+++ app/models/user.py
@@ -3,31 +3,48 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
+    id = db.Column(db.Integer, primary_key = True)
+    fullname = db.Column(db.String(255), nullable = False)
+    username = db.Column(db.String(40), nullable = False, unique = True)
+    email = db.Column(db.String(255), nullable = False, unique = True)
+    hashed_password = db.Column(db.String(255), nullable = False)
+    about = db.Column(db.String(255))
+    profilePicture = db.Column(db.String(225))
 
+    posts = db.relationship('Post', back_populates='users')
+    comments = db.relationship('Comment', back_populates='users')
+    likes = db.relationship('Like', back_populates='users')
 
-  @property
-  def password(self):
-    return self.hashed_password
+    @property
+    def password(self):
+        return self.hashed_password
 
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
 
-  @password.setter
-  def password(self, password):
-    self.hashed_password = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "fullname": self.fullname,
+            "username": self.username,
+            "email": self.email,
+            "about": self.about,
+            "profilePicture": self.profilePicture
+        }
 
-  def check_password(self, password):
-    return check_password_hash(self.password, password)
-
-
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    def to_joined_dict(self):
+        return {
+            "id": self.id,
+            "fullname": self.fullname,
+            "username": self.username,
+            "email": self.email,
+            "about": self.about,
+            "profilePicture": self.profilePicture.
+            "posts": [post.to_dict() for post in self.posts]
+        }
